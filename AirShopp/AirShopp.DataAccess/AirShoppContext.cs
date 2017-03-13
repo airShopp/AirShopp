3ca2e19d7d3999c7650cbc504dba37f0f7a546fd
Jasper Yue <JasperYue@augmentum.com.cn>
2017-03-13 11:01:05 +0800

    initial version of project

---
+++ AirShopp/AirShopp.DataAccess/AirShoppContext.cs
@@ -0,0 +1,18 @@
+﻿using AirShopp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirShopp.DataAccess
+{
+    public class AirShoppContext :DbContext
+    {
+        public AirShoppContext() : base("name = AirContext")
+        {
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
