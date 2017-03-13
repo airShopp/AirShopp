3da391a076b4c2ece6be3cb3512762113a82a25e
Amanda <936588182@qq.com>
2017-03-13 18:24:35 +0800

    add product model

---
+++ AirShopp/AirShopp.Domain/Product/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirShopp.Domain
+{
+    public class Product
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string ProductName { get; set; }
+        public long CategoryId { get; set; }
+        public long ProviderId { get; set; }
+        public int Storage { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public string KeepDate { get; set; }
+        public decimal Price { get; set; }
+        public int Supply { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category category { get; set; }
+
+        [ForeignKey("ProviderId")]
+        public virtual Provider provider { get; set; }
+    }
+}
