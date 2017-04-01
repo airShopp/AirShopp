68f925f5cf35a04e6083681e50fb695759ad4702
Jasper Yue <JasperYue@augmentum.com.cn>
2017-04-01 19:45:20 +0800

    Add model and QRCode function

---
+++ AirShopp/AirShopp.DataAccess/Mapping/AreaMapping.cs
@@ -0,0 +1,23 @@
+﻿using AirShopp.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirShopp.DataAccess.Mapping
+{
+    class AreaMapping : EntityTypeConfiguration<Area>
+    {
+        public AreaMapping()
+        {
+            HasKey(e => e.Id).Property(e => e.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(e => e.AreaId).IsRequired();
+            Property(e => e.AreaName).HasMaxLength(200).IsRequired();
+
+            HasRequired(e => e.City).WithMany(e => e.Areas).HasForeignKey(fk => fk.CityId);
+        }
+    }
+}
