d446b63d67a6750547d496ba6138cf21655f188a
Kenneth <2837344092@qq.com>
2017-04-10 19:29:37 +0800

    Refine

--- AirShopp/AirShopp.UI/App_Start/BundleConfig.cs
+++ AirShopp/AirShopp.UI/App_Start/BundleConfig.cs
@@ -18,9 +18,38 @@ namespace AirShopp.UI
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-            bundles.Add(new ScriptBundle("~/bundles/AirShopp").Include(
-                "~/Scripts/knockout-3.4.2.js", 
-                "~/Scripts/Init/test.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/AirShopp/JS").Include(
+                "~/Scripts/knockout-3.4.2.js",
+                "~/Scripts/Init/test.js",
+                "~/Scripts/MainJS/jquery-1.8.2.min.js",
+                "~/Scripts/MainJS/scripts.js",
+                "~/Scripts/MainJS/supersized-init.js",
+                "~/Scripts/MainJS/supersized.3.2.7.min.js"
+
+                ));
+            bundles.Add(new StyleBundle("~/bundles/AirShopp/Css").Include(
+                "~/Content/Css/reset.css",
+                "~/Content/Css/style.css",
+                "~/Content/Css/supersized.css"
+
+                ));
+            bundles.Add(new StyleBundle("~/bundles/HomePage/Css").Include(
+                "~/Content/Css/HomePage/bootstrap.css",
+                "~/Content/Css/HomePage/style.css",
+                "~/Content/Css/HomePage/megamenu.css"
+                ));
+            bundles.Add(new ScriptBundle("~/bundles/HomePage").Include(
+                "~/Scripts/MainJS/HomePage/jquery-1.11.1.min.js",
+                "~/Scripts/MainJS/HomePage/menu_jquery.js",
+                "~/Scripts/MainJS/HomePage/megamenu.js",
+                "~/Scripts/MainJS/HomePage/simpleCart.min.js",
+                "~/Scripts/MainJS/HomePage/scripts.js",
+                "~/Scripts/MainJS/HomePage/modernizr.custom.js",
+                "~/Scripts/MainJS/HomePage/move-top.js",
+                "~/Scripts/MainJS/HomePage/easing.js",
+                "~/Scripts/MainJS/HomePage/jquery.flexisel.js"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
