dc24bc7edf573880dae328f2a28a3771ca928c38
Kenneth <2837344092@qq.com>
2017-04-14 13:26:26 +0800

    Refine

---
+++ AirShopp/AirShopp.UI/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using AirShopp.UI.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AirShopp.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(HomeViewModel  model)
+        {
+            return View("ProductDetail", model);
+        }
+    }
+}
