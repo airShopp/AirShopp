f6133352d774786b49b53c9a53023b9ba7f9365f
Kenneth <2837344092@qq.com>
2017-04-17 19:12:43 +0800

    Refine home page

--- AirShopp/AirShopp.UI/Controllers/ProductController.cs
+++ AirShopp/AirShopp.UI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using AirShopp.UI.Models.ViewModel;
+﻿using AirShopp.UI.Models;
+using AirShopp.UI.Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,10 @@ namespace AirShopp.UI.Controllers
         public ActionResult Index(HomeViewModel  model)
         {
             return View("ProductDetail", model);
+        }
+        public ActionResult getProduct(long CategoryId)
+        {
+            return View();
         }
     }
 }
