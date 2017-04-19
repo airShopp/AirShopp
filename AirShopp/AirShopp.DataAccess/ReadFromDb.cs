1d52bd0d9b05593bb685dd1cc9ec588fa7ac7d7a
Administrator <Administrator@pc201503181634>
2017-04-19 22:44:15 +0800

    Add four forget file to commit

--- AirShopp/AirShopp.DataAccess/ReadFromDb.cs
+++ AirShopp/AirShopp.DataAccess/ReadFromDb.cs
@@ -6,6 +6,12 @@ namespace AirShopp.DataAccess
     public class ReadFromDb : IReadFromDb
     {
         public AirShoppContext _context = new AirShoppContext();
-        public IQueryable<Category> Categories => _context.Category;
+
+        // public IQueryable<Category> Categories => _context.Category;
+
+        public IQueryable<Category> Categories
+        {
+            get { return _context.Category; }
+        }
     }
 }
